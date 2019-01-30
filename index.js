051395c792026f7dd3716440ae827224f346e955
Caden Albaugh <cadenalbaugh@gmail.com>
2019-01-30 09:53:40 -0600

    feat: create initial google passport strategy implementation

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const passport = require("passport");
 const { Strategy: GoogleStrategy } = require("passport-google-oauth20");
+const { googleClientID, googleClientSecret } = require("./config/keys");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: googleClientID,
+      clientSecret: googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
 app.listen(PORT);
