a1db014ea644a9afcef17c9d228758aec329286e
Caden Albaugh <cadenalbaugh@gmail.com>
2019-01-31 13:26:16 -0600

    feat: add authorization logic via passport cookies

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
 const authRoutes = require("./routes/authRoutes");
+
+const THIRTY_DAYS = 30 * 24 * 60 * 60 * 1000;
 
 require("./models/User");
 require("./services/passport");
@@ -13,6 +17,16 @@ mongoose.connect(
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(
+  cookieSession({
+    maxAge: THIRTY_DAYS,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 authRoutes(app);
 
