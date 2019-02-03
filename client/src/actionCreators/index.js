f12d2d0dbfdf6071dbf6958064f7446c18721da7
Caden Albaugh <cadenalbaugh@gmail.com>
2019-02-03 13:32:30 -0600

    refactor: fetch user

--- client/src/actionCreators/index.js
+++ client/src/actionCreators/index.js
@@ -1,15 +1,13 @@
 import axios from "axios";
 import { FETCH_USER } from "../actions/index";
 
-const fetchUser = () => {
-  return async dispatch => {
-    try {
-      const currentUser = await axios.get("/api/current_user");
+export const fetchUser = () => async dispatch => {
+  try {
+    const res = await axios.get("/api/current_user");
 
-      dispatch({
-        type: FETCH_USER,
-        currentUser
-      });
-    } catch (error) {}
-  };
+    dispatch({
+      type: FETCH_USER,
+      currentUser: res.data
+    });
+  } catch (error) {}
 };
