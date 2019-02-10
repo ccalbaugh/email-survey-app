0b1ba0e0d73a4f620d65914a9e1bd33b4e8009ba
Caden Albaugh <cadenalbaugh@gmail.com>
2019-02-10 11:42:28 -0600

    feat: use requireLogin middleware in the stripe billing route handler

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,8 +1,9 @@
 const keys = require("../config/keys");
 const stripe = require("stripe")(keys.stripeSecretKey);
+const requireLogin = require("../middlewares/requireLogin");
 
 module.exports = app => {
-  app.post("/api/stripe", async (req, res) => {
+  app.post("/api/stripe", requireLogin, async (req, res) => {
     try {
       const charge = await stripe.charges.create({
         amount: 500,
@@ -11,7 +12,10 @@ module.exports = app => {
         source: req.body.id
       });
 
-      console.log(charge);
+      req.user.credits += 5;
+      const user = await req.user.save();
+
+      res.send(user);
     } catch (error) {}
   });
 };
