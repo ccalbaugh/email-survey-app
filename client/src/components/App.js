d229205b315a005d4fbaf8b7d525163401725374
Caden Albaugh <cadenalbaugh@gmail.com>
2019-02-10 09:30:55 -0600

    feat: use new user provider

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,24 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import { HeaderConnector } from "./Header/HeaderConnector";
 import { Dashboard } from "./Dashboard";
-import { Header } from "./Header";
 import { Landing } from "./Landing";
 import { SurveyNew } from "./SurveyNew";
+import { UserProviderConnector } from "./UserProvider/UserProviderConnector";
 
 export const App = () => {
   return (
-    <div className="container">
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={SurveyNew} />
-        </div>
-      </BrowserRouter>
-    </div>
+    <UserProviderConnector>
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <HeaderConnector />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    </UserProviderConnector>
   );
 };
