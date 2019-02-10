8d1d800733ed7b23307c77bb09888a5f1abca3c3
Caden Albaugh <cadenalbaugh@gmail.com>
2019-02-10 10:44:02 -0600

    feat: add billing route handlers to main server file

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 const authRoutes = require("./routes/authRoutes");
+const billingRoutes = require("./routes/billingRoutes");
 
 const THIRTY_DAYS = 30 * 24 * 60 * 60 * 1000;
 
@@ -13,10 +15,9 @@ const PORT = process.env.PORT || 5000;
 require("./models/User");
 require("./services/passport");
 
-mongoose.connect(
-  keys.mongoURI,
-  { useNewUrlParser: true }
-);
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
+
+app.use(bodyParser.json());
 
 app.use(
   cookieSession({
@@ -29,5 +30,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 authRoutes(app);
+billingRoutes(app);
 
 app.listen(PORT);
