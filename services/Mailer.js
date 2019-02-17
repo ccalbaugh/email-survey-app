89593556c8fe82c56c3245ba76bb511a40c326e9
Caden Albaugh <cadenalbaugh@gmail.com>
2019-02-17 10:48:53 -0600

    feat: create helper func to add recipients to the Mailer class

--- services/Mailer.js
+++ services/Mailer.js
@@ -28,6 +28,15 @@ class Mailer extends helper.Mail {
     trackingSettings.setClickTracking(clickTracking);
     this.addTrackingSettings(trackingSettings);
   }
+
+  addRecipients() {
+    const personalize = new helper.Personalization();
+
+    this.recipients.forEach(recipient => {
+      personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
+  }
 }
 
 module.exports = Mailer;
