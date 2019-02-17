ace68337450e10a793d7cdaa88e174ad49b1091a
Caden Albaugh <cadenalbaugh@gmail.com>
2019-02-17 17:19:26 -0600

    feat: add a route handler to thank the user for their input

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,7 +7,11 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
-  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+  app.get("/api/surveys/thanks", (req, res) => {
+    res.send("Thanks for voting!");
+  });
+
+  app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const {
       body: { title, subject, body, recipients },
       user: { id: _user }
@@ -23,6 +27,18 @@ module.exports = app => {
     });
 
     const mailer = new Mailer(survey, surveyTemplate(survey));
-    mailer.send();
+
+    try {
+      await mailer.send();
+      await survey.save();
+
+      req.user.credits -= 1;
+
+      const user = await req.user.save();
+
+      res.send(user);
+    } catch (error) {
+      res.status(422).send(error);
+    }
   });
 };
