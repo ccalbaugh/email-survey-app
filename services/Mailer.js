7b8348496960b8f5999da3b48774d6bfc3adc59a
Caden Albaugh <cadenalbaugh@gmail.com>
2019-02-17 11:02:15 -0600

    feat: add send function in Mailer class

--- services/Mailer.js
+++ services/Mailer.js
@@ -1,11 +1,12 @@
 const sendgrid = require("sendgrid");
-const { mail: helper } = sendgrid;
+const helper = sendgrid.mail;
 const keys = require("../config/keys");
 
 class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
 
+    this.sgApi = sendgrid(keys.sendGridKey);
     this.from_email = new helper.Email("no-reply@emailService.com");
     this.subject = subject;
     this.body = new helper.Content("text/html", content);
@@ -36,6 +37,17 @@ class Mailer extends helper.Mail {
       personalize.addTo(recipient);
     });
     this.addPersonalization(personalize);
+  }
+
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: "POST",
+      path: "/v3/mail/sned",
+      body: this.toJSON()
+    });
+
+    const response = this.sgApi.API(request);
+    return response;
   }
 }
 
