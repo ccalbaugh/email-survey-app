b99aa51d985ebf6539ff04abb953b0787d706e6d
Caden Albaugh <cadenalbaugh@gmail.com>
2019-02-17 10:44:15 -0600

    feat: add clickTracking and formatted address helper functions to Mailer

--- services/Mailer.js
+++ services/Mailer.js
@@ -10,6 +10,23 @@ class Mailer extends helper.Mail {
     this.subject = subject;
     this.body = new helper.Content("text/html", content);
     this.recipients = this.formatAddresses(recipients);
+
+    this.addContent(this.body);
+    this.addClickTracking();
+  }
+
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
+
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.clickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
   }
 }
 
